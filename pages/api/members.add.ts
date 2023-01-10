ec069d42a0056929972cc85560fdea9494887042
Hyeon Park <hkparkjs@gmail.com>
2023-01-10 15:27:39 +0900

    🚧 Add API for saving uid to database

---
+++ pages/api/members.add.ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import FirebaseAdmin from '@/models/firebase_admin';
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    uid,
+    email,
+    displayName,
+    photoURL
+  } = req.body;
+  if (uid === undefined || uid === null) {
+    res.status(400).json({ result: false, message: 'uid가 누락되었습니다.' });
+  }
+
+  try {
+    const addResult = await FirebaseAdmin.getInstance()
+      .Firebase.collection('members')
+      .doc(uid)
+      .set({
+        uid,
+        email: email ?? '',
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? ''
+      });
+    return res.status(200).json({ result: true, id: addResult });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ result: false }); 
+  }
+}
