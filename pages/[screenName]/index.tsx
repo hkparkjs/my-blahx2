8973f76d0832f28cf53868e3774265b5efa2ac0f
Hyeon Park <hkparkjs@gmail.com>
2023-01-11 15:31:10 +0900

    ✨ Add question textarea

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,6 +1,8 @@
 import { ServiceLayout } from '@/components/service_layout';
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from '@chakra-ui/react';
 import { NextPage } from 'next';
+import { useState } from 'react';
+import ResizeTextarea from 'react-textarea-autosize';
 
 const userInfo = {
   uid: 'test',
@@ -10,6 +12,8 @@ const userInfo = {
 };
 
 const UserHomePage: NextPage = function () {
+  const [message, setMessage] = useState('');
+  const toast = useToast();
   return (
     <ServiceLayout title="user home" minH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
@@ -22,6 +26,45 @@ const UserHomePage: NextPage = function () {
             </Flex>
           </Flex>
         </Box>
+      </Box>
+      <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
+        <Flex align="center" p="2">
+          <Avatar size="xs" src="http://bit.ly/broken-link" mr="2" />
+          <Textarea
+            bg="gray.100"
+            border="none"
+            placeholder="무엇이 궁금한가요?"
+            resize="none"
+            minH="unset"
+            overflow="hidden"
+            fontSize="xs"
+            mr="2"
+            maxRows={7}
+            as={ResizeTextarea}
+            value={message}
+            onChange={(e)=>{
+              if (e.currentTarget.value) {
+                const lineCount = (e.currentTarget.value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 1) + 1;
+                console.info(lineCount)
+                if (lineCount > 7) {
+                  // TODO: add toast
+                  return;
+                }
+              }
+              setMessage(e.currentTarget.value);
+            }}
+          />
+          <Button
+            disabled={message.length === 0}
+            bgColor="#FFB86C"
+            color="white"
+            colorScheme="yellow"
+            variant="solid"
+            size="sm"
+          >
+            등록
+          </Button>
+        </Flex>
       </Box>
     </ServiceLayout>
   );
