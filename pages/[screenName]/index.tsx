7cca711caf14a8139592b334406515c31149b112
Hyeon Park <hkparkjs@gmail.com>
2023-01-11 16:05:02 +0900

    ✨ Add anonymous button

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,5 +1,6 @@
 import { ServiceLayout } from '@/components/service_layout';
-import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from '@chakra-ui/react';
+import { useAuth } from '@/contexts/auth_user.context';
+import { Avatar, Box, Button, Flex, FormControl, FormLabel, Switch, Text, Textarea, useToast } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import { useState } from 'react';
 import ResizeTextarea from 'react-textarea-autosize';
@@ -13,7 +14,9 @@ const userInfo = {
 
 const UserHomePage: NextPage = function () {
   const [message, setMessage] = useState('');
+  const [isAnonymous, setAnonymous] = useState(true);
   const toast = useToast();
+  const { authUser } = useAuth();
   return (
     <ServiceLayout title="user home" minH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
@@ -26,45 +29,63 @@ const UserHomePage: NextPage = function () {
             </Flex>
           </Flex>
         </Box>
-      </Box>
-      <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
-        <Flex align="center" p="2">
-          <Avatar size="xs" src="http://bit.ly/broken-link" mr="2" />
-          <Textarea
-            bg="gray.100"
-            border="none"
-            placeholder="무엇이 궁금한가요?"
-            resize="none"
-            minH="unset"
-            overflow="hidden"
-            fontSize="xs"
-            mr="2"
-            maxRows={7}
-            as={ResizeTextarea}
-            value={message}
-            onChange={(e)=>{
-              if (e.currentTarget.value) {
-                const lineCount = (e.currentTarget.value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 1) + 1;
-                console.info(lineCount)
-                if (lineCount > 7) {
-                  // TODO: add toast
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
+          <Flex align="center" p="2">
+            <Avatar size="xs" src={isAnonymous ? 'http://bit.ly/broken-link' : authUser?.photoURL ?? 'http://bit.ly/broken-link'} mr="2" />
+            <Textarea
+              bg="gray.100"
+              border="none"
+              placeholder="무엇이 궁금한가요?"
+              resize="none"
+              minH="unset"
+              overflow="hidden"
+              fontSize="xs"
+              mr="2"
+              maxRows={7}
+              as={ResizeTextarea}
+              value={message}
+              onChange={(e)=>{
+                if (e.currentTarget.value) {
+                  const lineCount = (e.currentTarget.value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 1) + 1;
+                  console.info(lineCount)
+                  if (lineCount > 7) {
+                    // TODO: add toast
+                    return;
+                  }
+                }
+                setMessage(e.currentTarget.value);
+              }}
+            />
+            <Button
+              disabled={message.length === 0}
+              bgColor="#FFB86C"
+              color="white"
+              colorScheme="yellow"
+              variant="solid"
+              size="sm"
+            >
+              등록
+            </Button>
+          </Flex>
+          <FormControl display="flex" alignItems="center" mt="1" mx="2" pb="2">
+            <Switch
+              size="sm"
+              colorScheme="orange"
+              id="anonymous"
+              mr="1"
+              isChecked={isAnonymous}
+              onChange={() => {
+                if (authUser === null) {
+                  // TODO: show toast
+                  // toast({title: '로그인이 필요합니다', position: 'top-right'});
                   return;
                 }
-              }
-              setMessage(e.currentTarget.value);
-            }}
-          />
-          <Button
-            disabled={message.length === 0}
-            bgColor="#FFB86C"
-            color="white"
-            colorScheme="yellow"
-            variant="solid"
-            size="sm"
-          >
-            등록
-          </Button>
-        </Flex>
+                setAnonymous((prev) => !prev);
+              }}
+            />
+            <FormLabel htmlFor='anonymous' mb="0" fontSize="xx-small">Anonymous</FormLabel>
+          </FormControl>
+        </Box>
       </Box>
     </ServiceLayout>
   );
