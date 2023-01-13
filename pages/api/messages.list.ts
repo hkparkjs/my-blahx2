49a58681049d9233faf6080b159d74b7e7f75e47
Hyeon Park <hkparkjs@gmail.com>
2023-01-13 14:18:41 +0900

    ✨ Add API for getting messages

---
+++ pages/api/messages.list.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import checkSupportMethod from '@/controllers/error/check_support_method';
+import handleError from '@/controllers/error/handle_error';
+import MessageCtrl from '@/controllers/message.ctrl';
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  const supportMethod = ['GET'];   // add.ts내에서는 POST request만 지원할 것임.
+  try {
+    checkSupportMethod(supportMethod, method);
+    await MessageCtrl.list(req, res);
+  } catch (err) {
+    console.error(err);
+    // 에러 처리
+    handleError(err, res);
+  }
+} 
