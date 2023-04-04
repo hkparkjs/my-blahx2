519d6f9ecbff5f0e6040d2fecd5fb4c4cfed08a1
Hyeon Park <hkparkjs@gmail.com>
2023-04-04 15:14:00 +0900

    ✨ Implement processing message list using React Query

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -9,6 +9,7 @@ import axios, { AxiosResponse } from 'axios';
 import { InAuthUser } from '@/models/in_auth_user';
 import MessageItem from '@/components/message_item';
 import { InMessage } from '@/models/message/in_message';
+import { useQuery } from 'react-query';
 
 interface Props {
   userInfo: InAuthUser | null;
@@ -61,24 +62,6 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
   const [messageListFetchTrigger, setMessageListFetchTrigger] = useState(false);
   const toast = useToast();
   const { authUser } = useAuth();
-  async function fetchMessageList(uid: string) {
-    try {
-      const resp = await fetch(`/api/messages.list?uid=${uid}&page=${page}&size=10`);
-      if (resp.status === 200) {
-        const data: {
-          totalElements: number;
-          totalPages: number;
-          page: number;
-          size: number;
-          content: InMessage[];
-        } = await resp.json();
-        setTotalPages(data.totalPages);
-        setMessageList(prev => [...prev, ...data.content]);
-      }
-    } catch (err) {
-      console.error(err);
-    };
-  }
   async function fetchMessageInfo({ uid, messageId }: { uid: string; messageId: string }) {
     try {
       const resp = await fetch(`/api/messages.info?uid=${uid}&messageId=${messageId}`);
@@ -98,10 +81,27 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
       console.error(err);
     };
   }
-  useEffect(() => {
-    if (userInfo === null) return;
-    fetchMessageList(userInfo.uid);
-  }, [userInfo, messageListFetchTrigger, page]);
+  const messageListQueryKey = ['messageList', userInfo?.uid, page, messageListFetchTrigger];
+  useQuery(messageListQueryKey, async () => await axios.get<{
+    totalElements: number;
+    totalPages: number;
+    page: number;
+    size: number;
+    content: InMessage[];
+  }>(`/api/messages.list?uid=${userInfo?.uid}&page=${page}&size=10`),
+  {
+    keepPreviousData: true,
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      setTotalPages(data.data.totalPages);
+      if (page === 1) {
+        setMessageList([...data.data.content]);
+        return;
+      }
+      setMessageList(prev => [...prev, ...data.data.content]);
+    },
+  }
+  );
   if (userInfo === null) {
     return <p>사용자를 찾을 수 없습니다.</p>;
   }
@@ -175,7 +175,10 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
                   console.log('메세지 등록 실패');
                 }
                 setMessage('');
-                setMessageListFetchTrigger((prev) => !prev);
+                setPage(1);
+                setTimeout(() => {
+                  setMessageListFetchTrigger((prev) => !prev);
+                }, 50);
               }}
             >
               등록
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,22 @@ import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import type { AppProps /*, AppContext */ } from 'next/app';
 import { AuthUserProvider } from '@/contexts/auth_user.context';
+import { useRef } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
   return (
-    <ChakraProvider>
-      <AuthUserProvider>
-        <Component {...pageProps} />
-      </AuthUserProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClientRef.current}>
+      <ChakraProvider>
+        <AuthUserProvider>
+          <Component {...pageProps} />
+        </AuthUserProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
